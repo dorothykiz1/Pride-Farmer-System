7afdd142a4e9477764f32d76bf11342a8598ddff
@dorothykiz1 <kizdorothy@gmail.com>
2019-04-18 06:33:22 +0300

    Modify serializers.py file

--- backend/farmer_system/jica/serializers.py
+++ backend/farmer_system/jica/serializers.py
@@ -2,43 +2,43 @@ from rest_framework import serializers
 
 from .models import District,Subcounty,Parish,Farmer,Harvest,Officer,Season
  
-class DistrictSerializer(Serializers.ModelSerializer):
+class DistrictSerializer(serializers.ModelSerializer):
      
     class Meta:
         model= District
         fields = "__all__"
 
-class SubcountySerializer(Serializers.ModelSerializer):
+class SubcountySerializer(serializers.ModelSerializer):
      
     class Meta:
         model= Subcounty
         fields = "__all__"
 
-class ParishSerializer(Serializers.ModelSerializer):
+class ParishSerializer(serializers.ModelSerializer):
      
     class Meta:
         model= Parish
         fields = "__all__"
 
-class FarmerSerializer(Serializers.ModelSerializer):
+class FarmerSerializer(serializers.ModelSerializer):
      
     class Meta:
         model= Farmer
         fields = "__all__"
 
-class HarvestSerializer(Serializers.ModelSerializer):
+class HarvestSerializer(serializers.ModelSerializer):
      
     class Meta:
         model= Harvest
         fields = "__all__"
 
-class OfficerSerializer(Serializers.ModelSerializer):
+class OfficerSerializer(serializers.ModelSerializer):
      
     class Meta:
         model= Officer
         fields = "__all__"
 
-class SeasonSerializer(Serializers.ModelSerializer):
+class SeasonSerializer(serializers.ModelSerializer):
      
     class Meta:
         model= Season
